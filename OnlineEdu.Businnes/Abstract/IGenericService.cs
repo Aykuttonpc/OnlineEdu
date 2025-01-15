160ea9f1257c6a22b7440482522be13aae9f7da0
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-01-15 16:59:37 +0300

    UPDATE

--- OnlineEdu.Businnes/Abstract/IGenericService.cs
+++ OnlineEdu.Businnes/Abstract/IGenericService.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineEdu.Businnes.Abstract
 {
-    public interface IGenericService
+    public interface IGenericService<T> where T : class
     {
+        List<T> TGetList();
 
+        T TGetByFilter(Expression<Func<T, bool>> predicate);
+
+        T TGetById(int id);
+        void Tcreate(T entity);
+        void TUpdate(T entity);
+        void TDelete(int id);
+        int TCount();
+        int TFilterCount(Expression<Func<T, bool>> predicate);
+        List<T> TGetFilteredList(Expression<Func<T, bool>> predicate);
     }
 }
---
+++ OnlineEdu.Businnes/Concrete/GenericManager.cs
@@ -0,0 +1,64 @@
+﻿using OnlineEdu.Businnes.Abstract;
+using OnlineEdu.DataAcces.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.Businnes.Concrete
+{
+    public class GenericManager<T>(IRepository<T>  _repository) : IGenericService<T> where T : class
+    {
+
+
+
+        public int TCount()
+        {
+            return _repository.Count();
+        }
+
+        public void Tcreate(T entity)
+        {
+           _repository.create(entity);
+        }
+
+        public void TDelete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        public int TFilterCount(Expression<Func<T, bool>> predicate)
+        {
+             return _repository.FilterCount(predicate);
+        }
+
+        public T TGetByFilter(Expression<Func<T, bool>> predicate)
+        {
+            return _repository.GetByFilter(predicate);
+        }
+
+        public T TGetById(int id)
+        {
+            return _repository.GetById(id); 
+        }
+
+        public List<T> TGetFilteredList(Expression<Func<T, bool>> predicate)
+        {
+            return _repository.GetFilteredList(predicate);
+        }
+
+        public List<T> TGetList()
+        {
+            return _repository.GetList();
+        }
+
+        public void TUpdate(T entity)
+        {
+            _repository.Update(entity);
+
+
+        }
+    }
+}
