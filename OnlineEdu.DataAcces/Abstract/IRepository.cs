6e3b5ab9cfe1deb741b394d76ddf82acdd8f20ff
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-01-15 14:55:36 +0300

    update

---
+++ OnlineEdu.DataAcces/Abstract/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.DataAcces.Abstract
+{
+    public interface IRepository <T> where T : class
+    {
+        List <T> GetList ();
+
+        T GetByFilter(Expression<Func<T, bool>> predicate);
+
+        T GetById(int id);
+        void create(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        int Count();
+        int FilterCount(Expression<Func<T, bool>> predicate);
+        List<T> GetFilteredList(Expression<Func<T, bool>> predicate);
+
+
+
+
+
+    }
+}
---
+++ OnlineEdu.DataAcces/Repositories/GenericRepository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineEdu.DataAcces.Abstract;
+using OnlineEdu.DataAcces.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.DataAcces.Repositories
+{
+    public class GenericRepository<T>(OnlineEduContext _context) : IRepository<T> where T : class
+    {
+
+        //private readonly OnlineEduContext _context;
+
+        //public GenericRepository(OnlineEduContext context)
+        //{
+        //    _context = context;
+        //}
+
+
+        public DbSet<T> Table { get => _context.Set<T>(); }
+
+        public int Count()
+        {
+            return Table.Count();
+
+        }
+
+        public void create(T entity)
+        {
+           Table.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entity = Table.Find(id);
+            Table.Remove(entity);
+            _context.SaveChanges();
+
+        }
+
+        public int FilterCount(Expression<Func<T, bool>> predicate)
+        {
+            return Table.Where(predicate).Count();
+        }
+
+        public T GetByFilter(Expression<Func<T, bool>> predicate)
+        {
+            return Table.Where(predicate).FirstOrDefault();
+        }
+
+        public T GetById(int id)
+        {
+            return Table.Find(id);
+        }
+
+        public List<T> GetFilteredList(Expression<Func<T, bool>> predicate)
+        {
+           return Table.Where(predicate).ToList();
+        }
+
+        public List<T> GetList()
+        {
+           return Table.ToList();
+
+        }
+
+        public void Update(T entity)
+        {
+            Table.Update(entity);
+            _context.SaveChanges();
+
+        }
+    }
+}
