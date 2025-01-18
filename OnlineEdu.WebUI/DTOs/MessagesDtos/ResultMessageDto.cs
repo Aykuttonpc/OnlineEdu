e471243d5b62f1820807389873e6c18f2649438f
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-01-18 20:02:14 +0300

    update

---
+++ OnlineEdu.WebUI/DTOs/MessagesDtos/ResultMessageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineEdu.WebUI.DTOs.MessagesDtos
+{
+    public class ResultMessageDto
+    {
+        public int MessageId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string MessageContent { get; set; }
+    }
+}
