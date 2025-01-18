04b773592ae35bc72c81c522866466b155f18f31
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-01-18 16:32:39 +0300

    update

---
+++ OnlineEdu.API/Controllers/SocialMediasController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Businnes.Abstract;
+using OnlineEdu.DTO.DTOs.AboutDtos;
+using OnlineEdu.DTO.DTOs.SocialMediaDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediasController(IGenericService<SocialMedia> _socialNediaService,IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _socialNediaService.TGetList();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _socialNediaService.TGetById(id);
+            return Ok(value);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            _socialNediaService.TDelete(id);
+            return Ok("Sosyal medya Alanı Silindi");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateSocialMediaDto createSocialMediaDto)
+        {
+            var newValue = _mapper.Map<SocialMedia>(createSocialMediaDto);
+            _socialNediaService.Tcreate(newValue);
+            return Ok("Yeni Sosyal medya  alanı oluşturuldu");
+
+        }
+        [HttpPut]
+        public IActionResult Update(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            var value = _mapper.Map<SocialMedia>(updateSocialMediaDto);
+            _socialNediaService.TUpdate(value);
+            return Ok("Sosyal medya  alanı güncellendi");
+        }
+    }
+}
