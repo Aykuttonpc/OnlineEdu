05eff11cf3aa27982d937d2470736eb6d71f0521
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-01-21 03:14:46 +0300

    update

---
+++ OnlineEdu.WebUI/Validators/BlogCategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using OnlineEdu.WebUI.DTOs.BlogCategoryDtos;
+
+namespace OnlineEdu.WebUI.Validators
+{
+    public class BlogCategoryValidator :AbstractValidator<CreateBlogCategoryDto>
+    {
+        public BlogCategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Kategori adı boş olamaz");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Kategori adı en fazla 30 karakter olmalıdır");
+
+        }
+    }
+}
