faf62ef7e18ff92973d99bb1149b9298492c2d59
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-01-28 04:24:48 +0300

    video 45

--- OnlineEdu.WebUI/DTOs/UserDtos/AssignRoleDto.cs
+++ OnlineEdu.WebUI/DTOs/UserDtos/AssignRoleDto.cs
@@ -2,7 +2,7 @@
 {
     public class AssignRoleDto
     {
-
+        public int RoleId { get; set; }
         public string RoleName { get; set; }
         public string Email { get; set; }
         public bool RoleExist { get; set; }
