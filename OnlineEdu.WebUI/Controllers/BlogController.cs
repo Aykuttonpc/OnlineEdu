bfea4ad27526ae54604069c7eb27a1248da5a96a
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-02-03 22:12:31 +0300

    74

--- OnlineEdu.WebUI/Controllers/BlogController.cs
+++ OnlineEdu.WebUI/Controllers/BlogController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.SubscriberDtos;
+using OnlineEdu.WebUI.Helpers;
 
 namespace OnlineEdu.WebUI.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly HttpClient _client  = HttpClientInstance.CreateClient();
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> Subscribe(CreateSubscriberDto model)
+        {
+            await _client.PutAsJsonAsync("subscribers", model);
+            return NoContent();
         }
     }
 }
---
+++ OnlineEdu.WebUI/ViewComponents/Blog/_BlogSubscribe.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace OnlineEdu.WebUI.ViewComponents.Blog
+{
+    public class _BlogSubscribe:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
