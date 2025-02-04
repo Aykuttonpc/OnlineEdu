39a3adfc8223004fb80865cd7fa4a45a9a098ba5
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-02-04 19:18:48 +0300

    78

---
+++ OnlineEdu.WebUI/ViewComponents/Contact/_ContactInfo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OnlineEdu.WebUI.DTOs.ContactDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Contact
+{
+    public class _ContactInfo :ViewComponent
+    { 
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public  async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var values = await _client.GetFromJsonAsync<List<ResultContactDto>>("contacts");
+            return View(values);
+
+        }
+    }
+}
