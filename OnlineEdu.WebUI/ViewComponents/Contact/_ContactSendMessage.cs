d867620161d2d54bce34466195f427cf20eb19f5
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-02-04 05:11:35 +0300

    77

---
+++ OnlineEdu.WebUI/ViewComponents/Contact/_ContactSendMessage.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WebUI.ViewComponents.Contact
+{
+    public class _ContactSendMessage:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();  
+        }
+    }
+}
