9682844e92e4009cc0a4350440a6694d0630f622
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-02-04 20:39:10 +0300

    80

---
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/SubscriberController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.SubscriberDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+
+    public class SubscriberController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public  async Task<IActionResult> IndexAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultSubscriberDto>>("subscribers");
+            return View(values);
+        }
+
+        public async Task<IActionResult> DeleteSubscriber(int id)
+        {
+            await _client.DeleteAsync($"Subscribers/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+
+        public async Task<IActionResult> ChangeStatusSubscriber(int id)
+        {
+            var value = await _client.GetFromJsonAsync<UpdateSubscriberDto>($"Subscribers/{id}");
+            if (value.IsActivate)
+            {
+                value.IsActivate = false;
+            }else
+            {
+                value.IsActivate = true;
+            }
+            
+            await _client.PutAsJsonAsync("subscribers", value);
+
+            return RedirectToAction("Index");
+
+        }
+
+ 
+        
+    }
+}
