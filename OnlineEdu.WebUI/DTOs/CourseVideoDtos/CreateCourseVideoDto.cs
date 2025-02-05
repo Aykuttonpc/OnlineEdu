1674fac4a1feaffe9deea0044a17d8298f65527a
Aykut Cıncık <aykutcincik@ogr.eskisehir.edu.tr>
2025-02-05 03:26:08 +0300

    85

---
+++ OnlineEdu.WebUI/DTOs/CourseVideoDtos/CreateCourseVideoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineEdu.WebUI.DTOs.CourseVideoDtos
+{
+    public class CreateCourseVideoDto
+    {
+
+
+        public int CourseId { get; set; }
+
+        public string VideoUrl { get; set; }
+        public int VideoNumber { get; set; }
+    }
+}
